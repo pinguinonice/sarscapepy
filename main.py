26a2ce1f583c228e6a7afe50ed3c179a9b156626
Mohammad  Mohseni Aref <mohseni.aref@gmail.com>
2020-02-14 17:52:08 +0100

    reading file

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ Created on Tue Feb 11 16:16:02 2020
 import geopandas
 
 
-from sarscapepy import shape2grid, dispGrid
+from sarscapepy import shape2grid, dispGrid,read_file
 
 
 
@@ -18,7 +18,7 @@ from sarscapepy import shape2grid, dispGrid
 #filename= input('Enter Shape file name please : ')
 filename= 'D:/Philipp/5-Projects/STINGS/Idrija/Project 2020/PS_S44A/PSI_PS_processing/geocoding/PSI_PS_60_0_VD.shp'
 #filename='D:/Philipp/5-Projects/STINGS/Idrija/Project 2020/Idrija/PS_S22D/PSI_PS_processing/geocoding/PSI_PS_60_0_VD.shp'
-df = geopandas.read_file(filename)
+df=read_file(filename)
 #interpolate to grid
 grid=shape2grid(dataFrame=df, values='Velocity', gridSize=1/3600, LonMin=14.00,LonMax=14.06,LatMin=45.98,LatMax=46.04, method='linear')
 
