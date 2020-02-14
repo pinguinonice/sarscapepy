093291ee26933b90fa2c3cf60157cdc19c6b9c83
Philipp Schneider <philipp@ifp.uni-stuttgart.de>
2020-02-14 15:46:53 +0100

    reverse lat axis

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ from sarscapepy import shape2grid
 
 # read file
 #filename= input('Enter Shape file name please : ')
-#filename= 'D:/Philipp/5-Projects/STINGS/Idrija/Project 2020/PS_S44A/PSI_PS_processing/geocoding/PSI_PS_60_0_VD.shp'
-filename='D:/Philipp/5-Projects/STINGS/Idrija/Project 2020/Idrija/PS_S22D/PSI_PS_processing/geocoding/PSI_PS_60_0_VD.shp'
+filename= 'D:/Philipp/5-Projects/STINGS/Idrija/Project 2020/PS_S44A/PSI_PS_processing/geocoding/PSI_PS_60_0_VD.shp'
+#filename='D:/Philipp/5-Projects/STINGS/Idrija/Project 2020/Idrija/PS_S22D/PSI_PS_processing/geocoding/PSI_PS_60_0_VD.shp'
 df = geopandas.read_file(filename)
 #interpolate to grid
 grid=shape2grid(dataFrame=df, values='Velocity', gridSize=1/3600, LonMin=14.00,LonMax=14.06,LatMin=45.98,LatMax=46.04, method='linear')
@@ -39,10 +39,15 @@ velocity = georaster.SingleBandRaster.from_array(grid.get('Velocity'), grid.get(
 
 # plot
 fig, ax = plt.subplots()
+
 plt.imshow(np.array(base.r[:,:,:], dtype='uint8')  , alpha=1,extent=base.extent)
+plt.xlabel('Lon')
+plt.ylabel('Lat')
 plot_grid=plt.imshow(np.array(velocity.r)  , alpha=0.6, cmap='RdBu',extent=velocity.extent)
 plt.clim(-20,20) 
 cbar = fig.colorbar(plot_grid)
+
+plt.ylim(plt.ylim()[::-1])
 plt.show()
 
 
