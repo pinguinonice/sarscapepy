fe850984d462161233cc64fb4ad889fbfd901e51
Mohammad  Mohseni Aref <mohseni.aref@gmail.com>
2020-02-14 18:22:04 +0100

    Add header information and time series

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ Created on Tue Feb 11 16:16:02 2020
 import geopandas
 
 
-from sarscapepy import shape2grid, dispGrid,read_file
+from sarscapepy import shape2grid, dispGrid,read_file,headerinfo
 
 
 
@@ -18,7 +18,10 @@ from sarscapepy import shape2grid, dispGrid,read_file
 #filename= input('Enter Shape file name please : ')
 filename= 'D:/Philipp/5-Projects/STINGS/Idrija/Project 2020/PS_S44A/PSI_PS_processing/geocoding/PSI_PS_60_0_VD.shp'
 #filename='D:/Philipp/5-Projects/STINGS/Idrija/Project 2020/Idrija/PS_S22D/PSI_PS_processing/geocoding/PSI_PS_60_0_VD.shp'
+#read header and drop last coloumn!
 df=read_file(filename)
+#extract header information and time series
+hdinfo,tinfo,ts=headerinfo(dataFrame=df)
 #interpolate to grid
 grid=shape2grid(dataFrame=df, values='Velocity', gridSize=1/3600, LonMin=14.00,LonMax=14.06,LatMin=45.98,LatMax=46.04, method='linear')
 
