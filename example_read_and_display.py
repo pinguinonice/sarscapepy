a42774b8147b25aee3f309c44b7c4b8d800f3d51
Philipp Schneider <philipp@ifp.uni-stuttgart.de>
2020-02-27 15:19:33 +0100

    added decomposeTwoOrbits to sarscapepy

--- example_read_and_display.py
+++ example_read_and_display.py
@@ -11,8 +11,7 @@ from sarscapepy import shape2grid, dispGrid
 
 # read file
 #filename= input('Enter Shape file name please : ')
-filename= 'D:/Philipp/5-Projects/STINGS/Idrija/Project 2020/PS_S44A/PSI_PS_processing/geocoding/PSI_PS_60_0_VD.shp'
-#filename='D:/Philipp/5-Projects/STINGS/Idrija/Project 2020/Idrija/PS_S22D/PSI_PS_processing/geocoding/PSI_PS_60_0_VD.shp'
+filename= 'D:/Philipp/5-Projects/STINGS/Idrija/Project 2020/Idrija/PS_S44A/PSI_PS_processing/geocoding/PSI_PS_60_0.shp'
 
 df = geopandas.read_file(filename)
 
@@ -25,7 +24,5 @@ grid=shape2grid(dataFrame=df, values='Velocity', gridSize=1/3600, LonMin=14.00,L
 base_path="basemap/Idrija_14.tif"
 
 dispGrid(grid,layer_name='Velocity',base_path=base_path)
-
-
 
 
--- sarscapepy.py
+++ sarscapepy.py
@@ -137,7 +137,7 @@ def shape2grid(dataFrame,gridSize,values=None,LonMin=None,LonMax=None,LatMin=Non
         if type(dataFrame[value][1])!=np.float64 :
             continue
         
-        print("Interpolate: " + value)
+        print("Spatial Interpolate: " + value)
         grid_z0 = griddata(points, dataFrame[value], tuple(grid), method)
         #  mask missing values with NaNs
         grid_z0[mask] = np.nan
@@ -176,7 +176,7 @@ def dispGrid(grid,layer_name=None,base_path=None):
     
     # plot
     fig, ax = plt.subplots()
-    
+    t = plt.title(layer_name)
     # if basemap path is set
     if type(base_path) == str:
         # load basemap
@@ -197,5 +197,109 @@ def dispGrid(grid,layer_name=None,base_path=None):
     plt.ylim(plt.ylim()[::-1])
     plt.show()
            
-    
+"""
+decomposeTwoOrbits description:
+    following:  https://www.sciencedirect.com/science/article/pii/S0377027305003070?via%3Dihub
+Parameters: 
+    input:
+    grid_asc: orbit1 (output dict from shape2grid)
+    grid_dsc: orbit2 (output dict from shape2grid)
+
+    output
+    grid_vert: vertical grid (up down) horizonata
+    grid_horz: horizontal grid (east west)
+"""    
   
+def decomposeTwoOrbits(grid_asc, grid_dsc, layer_name):
+    import numpy as np
+    
+    # check input
+    # assert len(grid_asc)==len(grid_dsc), 'dicts (grids) have different length' # error for wrong input
+    
+    # check if A_LOS and I_ALOS exists
+    
+    # check if layer_name exists in both grids
+    
+    # check if layer_name is np layer
+
+    
+    # get both layers 
+    layer_asc= grid_asc.get(layer_name).flatten()
+    layer_dsc= grid_dsc.get(layer_name).flatten()
+    
+    # assign empyte outpu
+    layer_east=np.full_like(layer_asc,np.nan)
+    layer_vert=np.full_like(layer_asc,np.nan)
+
+    
+    AZ_asc=grid_asc.get('LOS_Az').flatten()/180*np.pi
+    IN_asc=grid_asc.get('LOS_In').flatten()/180*np.pi
+    AZ_dsc=grid_dsc.get('LOS_Az').flatten()/180*np.pi
+    IN_dsc=grid_dsc.get('LOS_In').flatten()/180*np.pi   
+    
+        #for each pixel   
+    for i in range(0, len(AZ_asc)-1):   
+        # check for nan
+        if np.isnan( AZ_asc[i]) or np.isnan( AZ_dsc[i]):
+            continue
+        ''' this needs to be rethought since I only implemented a approximation 
+            as in https://www.sciencedirect.com/science/article/pii/S0377027305003070?via%3Dihub
+        '''
+        # build A matrix
+        # A=np.array([[-np.cos( IN_asc[i]), np.sin( IN_asc[i])*np.cos( AZ_asc[i]) ],
+        #             [-np.cos( IN_dsc[i]), np.sin( IN_dsc[i])*np.cos( AZ_dsc[i]) ]] )
+        # # build observations
+        # b=np.array([layer_asc[i],layer_dsc[i]])
+        # x = np.matmul(A, b)
+        
+        # layer_vert[i]= x[0]      
+        # layer_east[i]= x[1]
+        
+        layer_vert[i]= (layer_dsc[i]+layer_asc[i])/(2*np.sin( IN_asc[i]))    
+        layer_east[i]= (layer_dsc[i]-layer_asc[i])/(2*np.cos( IN_asc[i]))
+        
+        # print('-------------')        
+        # print('AZ_dsc=',AZ_dsc[i])
+        # print('AZ_asc=',AZ_asc[i])
+        # print('IN_dsc=',IN_dsc[i])
+        # print('IN_asc=',IN_asc[i])
+        # print('A=',A)
+
+        # print('x=',x)
+        # print('b=',b)
+        # print('layer_east[i]=',layer_east[i])
+        # print('layer_vert[i]=',layer_vert[i])
+        
+         
+        
+        # to vert and horzreshape  
+         #"assign to final output"
+        grid_asc.update( {layer_name+'_vert': layer_vert.reshape(grid_asc.get(layer_name).shape)} )
+        grid_asc.update( {layer_name+'_east': layer_east.reshape(grid_asc.get(layer_name).shape)} )
+        
+        grid_dsc.update( {layer_name+'_vert': layer_vert.reshape(grid_dsc.get(layer_name).shape)} )
+        grid_dsc.update( {layer_name+'_east': layer_east.reshape(grid_dsc.get(layer_name).shape)} )
+
+        
+
+    
+    
+    return grid_asc, grid_dsc
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
